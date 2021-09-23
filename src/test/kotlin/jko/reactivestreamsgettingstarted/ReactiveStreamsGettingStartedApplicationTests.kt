fea707cfe4ed202a0b5d1285a966ebaf7e38755e
junhee-ko <junheee.ko@gmail.com>
2021-09-23 22:31:33 +0900

    first commit

---
+++ src/test/kotlin/jko/reactivestreamsgettingstarted/ReactiveStreamsGettingStartedApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.reactivestreamsgettingstarted
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveStreamsGettingStartedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
